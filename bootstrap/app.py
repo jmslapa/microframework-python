97b896c7e121ed14e91f479f11bc46d63c1bd29a
João Manuel dos Santos Lapa <joaomanoel1996@hotmail.com>
2021-06-13 19:40:47 -0300

    O request agora é inicalizado no construtor de Application, as rotas sao recebidas por parâmetro pelo construtor e  o path e method são fornecidos pelo request

--- bootstrap/app.py
+++ bootstrap/app.py
@@ -1,3 +1,4 @@
+from router.interfaces import RouteComponent
 from http.payload import ResponsePayload
 from support.request import Request
 from http.response import html
@@ -5,17 +6,18 @@ from router.route import Route
 
 class Application():
 
-    routes = [
-        Route.group([
-            Route.get('/', 'IndexController', 'index'),
-            Route.get('/show/', 'IndexController', 'show')
-        ]).prefix('api').namespace('controllers.index')
-    ]
+    __routes = None
+    __request = None
 
-    def run(self, method, path) -> ResponsePayload:
-        path = path[:-1] if path[-1] == '/' else path
-        for route in self.routes:
-            if route.check(method, path):
+    def __init__(self, environ: dict, routes: "list[RouteComponent]") -> None:
+        self.__request = Request.getInstance().setEnv(environ)
+        self.__routes = routes
+
+    def run(self) -> ResponsePayload:
+
+        path = self.__request.path[:-1] if self.__request.path[-1] == '/' else self.__request.path
+        for route in self.__routes:
+            if route.check(self.__request.method, path):
                 return route.handle()
         return html('<h2>404: Sorry, endpoint not found.</h2>', 404)
         

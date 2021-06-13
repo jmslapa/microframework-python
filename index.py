6f24783844c58e529648155a3bbb0a23d0736c60
João Manuel dos Santos Lapa <joaomanoel1996@hotmail.com>
2021-06-13 13:01:34 -0300

    Funcao application agora trabalha com um ResponsePayload

--- index.py
+++ index.py
@@ -4,6 +4,6 @@ from bootstrap.app import Application
 def application(environ, start_response):
     request = Request.getInstance()
     request.setEnv(environ)
-    body, status, headers = Application().run(environ.get('REQUEST_METHOD'), environ.get('PATH_INFO'))
-    start_response(status, headers)
-    return [body]
+    payload = Application().run(environ.get('REQUEST_METHOD'), environ.get('PATH_INFO'))
+    start_response(payload.status, payload.headers)
+    return [payload.body]

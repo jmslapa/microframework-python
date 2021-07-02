4375d61d935655d445fb11393e869f7006513f15
João Manuel dos Santos Lapa <joaomanoel1996@hotmail.com>
2021-07-01 21:38:23 -0300

    Adicionada mensagem padrao caso nao exista parametro

--- src/controllers/test.py
+++ src/controllers/test.py
@@ -3,7 +3,7 @@ from mf.http.controller import Controller
 class TestController(Controller):
 
     def index(self):
-        return self._response.json({'message': self._request.body.get('example')})
+        return self._response.json({'message': self._request.body.get('example') or 'Mensagem padrão.'})
 
     def show(self):
         return self._response.json('show')
